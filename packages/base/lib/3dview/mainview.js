c75089405a9fd118a081d2d8cf94aa53234a7d98
xidianWQ <wanqiukong@gmail.com>
2026-03-12 11:42:57 +0800

    修改导出缩略图的背景色

--- packages/base/lib/3dview/mainview.js
+++ packages/base/lib/3dview/mainview.js
@@ -1430,8 +1430,14 @@ export class MainView extends React.Component {
                     this._renderer.setSize(originalSize.x * scaleFactor, originalSize.y * scaleFactor, false);
                     
                     // 截取缩略图, 强制渲染一次以确保缓冲区有最新的图像
+                    // 保存原始背景色并临时设置为 #f0f0f0
+                    const originalClearColor = this._renderer.getClearColor().clone();
+                    const originalClearAlpha = this._renderer.getClearAlpha();
+                    this._renderer.setClearColor(0xf0f0f0);
                     this._renderer.render(this._scene, this._camera);
                     const thumbnail = this._renderer.domElement.toDataURL('image/png');
+                    // 恢复原始背景色
+                    this._renderer.setClearColor(originalClearColor, originalClearAlpha);
                     this._renderer.setSize(originalSize.x, originalSize.y, false);
                     // 释放信号 (用于保存到后端/本地文件系统)
                     const filename = `${new Date().getTime()}.glb`;
